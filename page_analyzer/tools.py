6a4a7909da3c78339af3d9ba67bb02d00e2f7790
MaxKtv <maximkutiaev@outlook.com>
2024-09-05 15:54:15 +1000

    refactor(rename validator.py)/feat(dictionarize_soup_url) tools.py

---
+++ page_analyzer/tools.py
@@ -0,0 +1,33 @@
+from typing import Dict, Any
+from validators import url as url_validator
+from requests import get
+from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
+
+
+def is_valid_url(url: str) -> bool | ValueError:
+    if not url_validator(url) and len(url) >= 255:
+        raise ValueError("Invalid URL")
+    return True
+
+
+def dictionarize_soup_url(url: str) -> Dict[str, Any]:
+    try:
+        req = get(url, timeout=10)
+        req.raise_for_status()
+    except RequestException as e:
+        raise Exception(f"Error fetching the URL: {e}")
+
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    title = soup.title.get_text(strip=True) if soup.title else None
+    h1 = soup.h1.get_text(strip=True) if soup.h1 else None
+    meta_description_tag = soup.find('meta', {'name': 'description'})
+    description = meta_description_tag.get('content', '').strip() if meta_description_tag else None
+
+    return {
+        'status_code': req.status_code,
+        'h1': h1,
+        'title': title,
+        'description': description,
+    }
--- page_analyzer/validator.py
+++
@@ -1,7 +0,0 @@
-from validators import url as url_validator
-
-def is_valid_url(url):
-    if url_validator(url) and len(url) < 255:
-        return url
-    else:
-        raise ValueError("Invalid URL")
