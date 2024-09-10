2473ead619dbca09ee8cfe02fe63160233052e51
MaxKtv <maximkutiaev@outlook.com>
2024-09-10 15:58:51 +1000

    ref/doc app.py, db.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,6 @@
 from os import getenv
 from typing import Tuple
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, ParseResult
 from dotenv import load_dotenv
 from validators import url as url_validator
 from flask import (
@@ -10,7 +10,8 @@ from flask import (
     url_for,
     flash,
     request,
-    Response
+    Response,
+    abort
 )
 from .tools import dictionarize_soup_url
 from .db import (
@@ -31,21 +32,45 @@ app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 @app.route('/')
 def home() -> str:
+    """
+    Renders the homepage.
+
+    Returns:
+        str: The rendered HTML content for the homepage.
+    """
     return render_template('index.html')
 
 
 @app.route('/urls/<int:url_id>')
 def specific_url(url_id: int) -> str | Response:
+    """
+    Displays a specific URL by its ID, along with its checks.
+
+    Args:
+        url_id (int): The ID of the URL to display.
+
+    Returns:
+        str: The rendered HTML content for the URL page.
+        Response: Redirects to the homepage with
+        a flash message if the URL is not found.
+    """
+
     connection = connect_to_db()
     url, checks = fetch_url_by_id(url_id, connection)
     if not url:
-        flash('Сайт не найден', 'danger')
-        return redirect(url_for('home'))
+        abort(404, description="Сайт не найден")
     return render_template('urls/index.html', url=url, checks=checks)
 
 
 @app.route('/urls')
 def list_urls() -> str:
+    """
+    Displays a list of all URLs and their checks.
+
+    Returns:
+        str: The rendered HTML content listing all URLs.
+    """
+
     connection = connect_to_db()
     urls = fetch_all_urls(connection)
     return render_template('urls/urls.html', urls=urls)
@@ -53,13 +78,30 @@ def list_urls() -> str:
 
 @app.route('/urls', methods=['POST'])
 def add_new_url() -> Response | Tuple[str, int]:
+    """
+    Adds a new URL to the database via a form submission.
+
+    Validates the URL format, checks if it already exists, and adds it if not.
+
+    Returns:
+        Response: A redirect to the URL page if the URL is successfully added.
+        Tuple[str, int]: Renders the homepage with
+        a 422 status if the URL is invalid.
+    """
+
     url = request.form['url']
 
-    if not url_validator(url) and not len(url) >= 255:
+    if not url_validator(url) or len(url) >= 255:
         flash('Некорректный URL', 'danger')
         return render_template('index.html'), 422
 
-    normalized_url = urlunparse(urlparse(url)[:2] + ('', '', '', ''))
+    parsed_url = urlparse(url)
+    normalized_url = urlunparse(ParseResult(scheme=parsed_url.scheme,
+                                            netloc=parsed_url.netloc,
+                                            path='',
+                                            params='',
+                                            query='',
+                                            fragment=''))
 
     connection = None
     try:
@@ -90,13 +132,27 @@ def add_new_url() -> Response | Tuple[str, int]:
 
 @app.route('/urls/<int:url_id>/check', methods=['POST'])
 def add_check_url(url_id: int) -> Response:
+    """
+    Adds a new check for the specified URL.
+
+    Fetches the URL data, extracts relevant information,
+    and inserts a new check record.
+
+    Args:
+        url_id (int): The ID of the URL being checked.
+
+    Returns:
+        Response: A redirect to the URL page
+        after the check is successfully added.
+    """
+
     connection = None
     try:
         connection = connect_to_db()
-        url_data = fetch_url_by_id(url_id, connection)[0]
-        url = url_data[1]
-        dict_url_data = dictionarize_soup_url(url)
-        add_url_to_check(dict_url_data, url_id, connection)
+        url_data, _ = fetch_url_by_id(url_id, connection)
+        url = url_data['name']
+        soup_url_data = dictionarize_soup_url(url)
+        add_url_to_check(soup_url_data, url_id, connection)
         connection.commit()
         flash('Страница успешно проверена', 'success')
     except Exception:
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,23 +1,53 @@
 from datetime import datetime
 import os
-from typing import Tuple
+from typing import Tuple, List, Dict, Any
 
 from dotenv import load_dotenv
 from psycopg2 import extras, connect
+from psycopg2.extensions import connection
+
 
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-def connect_to_db():
+def connect_to_db() -> connection:
+    """
+    Establishes a connection to the database.
+
+    Loads environment variables from the `.env` file,
+    retrieves the database URL,
+    and creates a connection to the database using psycopg2.
+
+    Returns:
+        connection (connection): The database connection.
+
+    Raises:
+        ConnectionError: If the database connection cannot be established.
+    """
+
     try:
         return connect(DATABASE_URL, cursor_factory=extras.DictCursor)
     except Exception as e:
-        raise f'Could not connect to database: {e}'
+        raise ConnectionError(f'Could not connect to database: {e}')
 
 
-def add_url(url, conn):
+def add_url(url: str, conn: connection) -> int:
+    """
+    Adds a new URL to the `urls` table.
+
+    Args:
+        url (str): The URL to be saved in the database.
+        conn (connection): Active database connection.
+
+    Returns:
+        int: The ID of the newly added URL.
+
+    Raises:
+        Exception: If an error occurs during the SQL execution.
+    """
+
     with conn.cursor() as curs:
         curs.execute(
             'INSERT INTO urls (name, created_at) '
@@ -28,7 +58,19 @@ def add_url(url, conn):
     return url_id
 
 
-def fetch_all_urls(conn):
+def fetch_all_urls(conn: connection) -> List[Tuple[Any, ...]]:
+    """
+    Retrieves a list of all URLs from the database
+    with associated check information.
+
+    Args:
+        conn (connection): Active database connection.
+
+    Returns:
+        List[Tuple[Any, ...]]: A list of URLs and their checks,
+        ordered by creation date.
+    """
+
     with conn.cursor() as curs:
         curs.execute(
             'SELECT urls.id, urls.name, url_checks.created_at, '
@@ -41,7 +83,18 @@ def fetch_all_urls(conn):
     return urls
 
 
-def fetch_url_name_by_id(url_id, conn):
+def fetch_url_name_by_id(url_id: int, conn: connection) -> str | None:
+    """
+    Fetches the name (URL) by its ID.
+
+    Args:
+        url_id (int): The ID of the URL in the database.
+        conn (connection): Active database connection.
+
+    Returns:
+        str: The URL if found, None otherwise.
+    """
+
     with conn.cursor() as curs:
         curs.execute(
             'SELECT name FROM urls WHERE id = %s;',
@@ -50,24 +103,68 @@ def fetch_url_name_by_id(url_id, conn):
     return result['name'] if result else None
 
 
-def fetch_url_by_id(url_id, conn) -> Tuple:
+def fetch_url_by_id(url_id: int, conn: connection) \
+        -> Tuple[Dict[str, Any], List]:
+    """
+    Retrieves data about the URL and its checks by URL ID.
+
+    Args:
+        url_id (int): The ID of the URL in the database.
+        conn (connection): Active database connection.
+
+    Returns:
+        Tuple[Dict[str, Any], List]: A tuple containing the URL data
+        and a list of checks.
+    """
+
     with conn.cursor() as curs:
         curs.execute('SELECT * FROM urls WHERE id = %s;', (url_id,))
         url_data = curs.fetchone()
+        url_dicted_data = {
+            'id': url_data[0],
+            'name': url_data[1],
+            'created_at': url_data[2]
+        }
+
         curs.execute('SELECT * FROM url_checks WHERE url_id = %s '
                      'ORDER BY created_at DESC;', (url_id,))
         checks = curs.fetchall()
-    return url_data, checks
+
+    return url_dicted_data, checks
 
 
-def url_exists(url, conn) -> Tuple | None:
+def url_exists(url: str, conn: connection) -> Tuple | None:
+    """
+    Checks if a URL already exists in the database.
+
+    Args:
+        url (str): The URL to check.
+        conn (connection): Active database connection.
+
+    Returns:
+        tuple | None: A dict with URL data if it exists, None otherwise.
+    """
+
     with conn.cursor() as curs:
         curs.execute('SELECT * FROM urls WHERE name = %s;', (url,))
         result = curs.fetchone()
     return result
 
 
-def add_url_to_check(data, url_id, conn):
+def add_url_to_check(data: Dict[str, Any], url_id: int, conn) -> None:
+    """
+    Adds a URL check entry to the `url_checks` table.
+
+    Args:
+        data (Dict[str, Any]): A dictionary containing data
+        status code, h1, title, description.
+        url_id (int): The ID of the URL being checked.
+        conn (connection): Active database connection.
+
+    Returns:
+        None
+    """
+
     with conn.cursor() as curs:
         curs.execute(
             'INSERT INTO url_checks '
